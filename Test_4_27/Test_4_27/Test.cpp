9a7d7dff94d1871c44627c22e575dd0dc73d4fa2
whjlearn <858992079@qq.com>
2023-04-27 21:35:01 +0800

    面对对象

---
+++ Test_4_27/Test_4_27/Test.cpp
@@ -0,0 +1,48 @@
+//#include<iostream>
+//using namespace std;
+//class A
+//{
+//public:
+//	A(int a)
+//		:_a(a)
+//	{}
+//private:
+//	int _a;
+//};
+//class B
+//{
+//public:
+//	B(int a, int ref)
+//		:_aobj(a)
+//		, _ref(ref)
+//		, _n(10)
+//	{}
+//private:
+//	A _aobj; // 没有默认构造函数
+//	int& _ref; // 引用
+//	const int _n; // const
+//};
+//class Time
+//{
+//public:
+//	Time(int hour = 0)
+//		:_hour(hour)
+//	{
+//		cout << "Time()" << endl;
+//	}
+//private:
+//	int _hour;
+//};
+//class Date
+//{
+//public:
+//	Date(int day)
+//	{}
+//private:
+//	int _day;
+//	Time _t;
+//};
+//int main()
+//{
+//	Date d(1);
+//}
