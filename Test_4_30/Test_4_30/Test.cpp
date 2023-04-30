3d29bcb11ed04e88f242a08990aa19cffc181e81
whjlearn <858992079@qq.com>
2023-04-30 21:46:39 +0800

    c++初阶

---
+++ Test_4_30/Test_4_30/Test.cpp
@@ -0,0 +1,23 @@
+#include <time.h>
+#include<iostream>
+using namespace std;
+struct A { int a[10000]; };
+void TestFunc1(A a) {}
+void TestFunc2(A& a) {}
+void TestRefAndValue()
+{
+	A a;
+	// 以值作为函数参数
+	size_t begin1 = clock();
+	for (size_t i = 0; i < 10000; ++i)
+		TestFunc1(a);
+	size_t end1 = clock();
+	// 以引用作为函数参数
+	size_t begin2 = clock();
+	for (size_t i = 0; i < 10000; ++i)
+		TestFunc2(a);
+	size_t end2 = clock();
+	// 分别计算两个函数运行结束后的时间
+	cout << "TestFunc1(A)-time:" << end1 - begin1 << endl;
+cout << "TestFunc2(A&)-time:" << end2 - begin2 << endl;
+}int main(){	return 0;}
